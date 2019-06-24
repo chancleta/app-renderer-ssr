cb40030beea6dd224b4b0acf261f97196514df68
Luis Pena <ljpenaurena@gmail.com>
2019-06-24 16:33:03 -0400

    working on something;

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,11 +1,14 @@
 export const FETCH_USERS = 'FETCH_USERS';
+import {auth} from 'advertor-protobuf';
 
 export const fetchUsers = () => {
   return async (dispatch, getState, api) => {
-    const res = await api.get('https://react-ssr-api.herokuapp.com/users');
-    dispatch({
-      type: FETCH_USERS,
-      payload: res,
-    });
+    const res = await api.get('http://localhost:8080/user/all');
+    const bitArray = new TextEncoder().encode(res.data);
+    console.log(auth.Credentials.decode(bitArray));
+    // dispatch({
+    //   type: FETCH_USERS,
+    //   payload: res,
+    // });
   };
 };
